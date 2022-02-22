c1b2350c399e6ee3b63905a37e1b1f1520dcad29
thanhnhadv10 <50938924+thanhnhadv10@users.noreply.github.com>
2022-02-22 20:54:33 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ const CONFIG = {
     introTitle: 'Thu Hải',
     introDesc: `Trái đất vốn rất lạ thường
     Mà sao em cứ lạc đường trong trái tim anh`,
-    btnIntro: '^^Like HiHi^^',
+    btnIntro: '^^LikeHiHi^^',
     title: 'Anh rất hạnh phúc vì được gặp em, em là điều tuyệt vời nhất trong cuộc sống của anh',
     desc: 'Anh rất hạnh phúc vì được gặp em, em là điều tuyệt vời nhất trong cuộc sống của anh',
     btnYes: 'Thích lắm <33',
