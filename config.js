12515907288a0cbf92741445f37fb095fce6d2fe
thanhnhadv10 <50938924+thanhnhadv10@users.noreply.github.com>
2022-02-22 19:45:33 +0700

    update config
    update

--- config.js
+++ config.js
@@ -1,20 +1,16 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Thu Hải',
+    introDesc: `Trái đất vốn rất lạ thường
+    Mà sao em cứ lạc đường trong trái tim anh`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Anh rất hạnh phúc vì được gặp em, em là điều tuyệt vời nhất trong cuộc sống của anh',
+    desc: 'Anh rất hạnh phúc vì được gặp em, em là điều tuyệt vời nhất trong cuộc sống của anh',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    question: 'E làm người yêu của anh nha',
+    btnReply: 'Gửi câu trả lời cho bạn <3',
+    reply: 'A rất vui khi e đền được đây, I LOVE YOU',
+    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
+    messLink: 'https://github.com/nhahuynh/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
